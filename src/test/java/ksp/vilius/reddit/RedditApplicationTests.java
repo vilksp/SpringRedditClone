09a3f7abfd56556b31a21d28e4f9578858928da5
vilksp <v.kasperavicius1@gmail.com>
2021-03-18 14:48:12 +0200

    initial commit

---
+++ src/test/java/ksp/vilius/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
