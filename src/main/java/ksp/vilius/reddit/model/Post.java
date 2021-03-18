b94c2812ce227176c2c43289b218799128869de3
vilksp <v.kasperavicius1@gmail.com>
2021-03-18 17:24:21 +0200

    base model classes added

--- src/main/java/ksp/vilius/reddit/model/Post.java
+++ src/main/java/ksp/vilius/reddit/model/Post.java
@@ -1,2 +1,40 @@
-package ksp.vilius.reddit.model;public class Post {
+package ksp.vilius.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty!")
+    private String postName;
+    @Nullable
+    private String url;
+    @Lob
+    private String description;
+    private Integer voteCount = 0;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
 }
---
+++ src/main/java/ksp/vilius/reddit/model/Subreddit.java
@@ -0,0 +1,34 @@
+package ksp.vilius.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Subreddit {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotBlank(message = "Community name is required")
+    private String name;
+    @NotBlank(message = "Description is required")
+    private String description;
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    private User user;
+}
