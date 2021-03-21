e3b796d47bb7d085aa2115031a50aa394d80f527
vilksp <v.kasperavicius1@gmail.com>
2021-03-21 23:26:02 +0200

    fix

--- src/main/java/ksp/vilius/reddit/repositories/VerificationTokenRepository.java
+++ src/main/java/ksp/vilius/reddit/repositories/VerificationTokenRepository.java
@@ -1,9 +1,9 @@
 package ksp.vilius.reddit.repositories;
 
-import ksp.vilius.reddit.model.User;
+import ksp.vilius.reddit.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
