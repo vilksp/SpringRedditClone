bec1fab8c44e40697d2e7f0f945da030e0b1be20
vilksp <v.kasperavicius1@gmail.com>
2021-03-21 23:14:33 +0200

    password encode bean added

--- src/main/java/ksp/vilius/reddit/config/SecurityConfig.java
+++ src/main/java/ksp/vilius/reddit/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package ksp.vilius.reddit.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
