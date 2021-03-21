8834407a2448edab1c59e94b3e7887a2dfd93d50
vilksp <v.kasperavicius1@gmail.com>
2021-03-21 23:14:50 +0200

    dto class added for registering new user

--- src/main/java/ksp/vilius/reddit/dto/RegisterRequest.java
+++ src/main/java/ksp/vilius/reddit/dto/RegisterRequest.java
@@ -1,2 +1,15 @@
-package ksp.vilius.reddit.dto;public class RegisterRequest {
+package ksp.vilius.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String email;
+    private String username;
+    private String password;
 }
