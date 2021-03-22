7b01ecc8049615873c18adc375ef95cde6a49c61
vilksp <v.kasperavicius1@gmail.com>
2021-03-22 14:52:36 +0200

    Signup method added

--- src/main/java/ksp/vilius/reddit/controller/AuthController.java
+++ src/main/java/ksp/vilius/reddit/controller/AuthController.java
@@ -1,2 +1,28 @@
-package ksp.vilius.reddit.controller;public class AuthController {
+package ksp.vilius.reddit.controller;
+
+import ksp.vilius.reddit.dto.RegisterRequest;
+import ksp.vilius.reddit.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User registration successful", HttpStatus.OK);
+    }
+
 }
--- src/main/java/ksp/vilius/reddit/service/AuthService.java
+++ src/main/java/ksp/vilius/reddit/service/AuthService.java
@@ -1,2 +1,54 @@
-package ksp.vilius.reddit.service;public class AuthService {
+package ksp.vilius.reddit.service;
+
+import ksp.vilius.reddit.dto.RegisterRequest;
+import ksp.vilius.reddit.model.NotificationEmail;
+import ksp.vilius.reddit.model.User;
+import ksp.vilius.reddit.model.VerificationToken;
+import ksp.vilius.reddit.repositories.UserRepository;
+import ksp.vilius.reddit.repositories.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+        mailService.sendEmail(
+                new NotificationEmail(
+                        "Please activate your account!", user.getEmail(),
+                        "Thank you for signing up to Spring Reddit clone, please click on the url below to active your account: "
+                                + "http://localhost:8080/api/auth/accountVerification/" + token));
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
 }
