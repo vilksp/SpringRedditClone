5eb8999d0180fbc0013c22806d1916f276e2665f
vilksp <v.kasperavicius1@gmail.com>
2021-03-22 14:52:15 +0200

    mail sending service added

--- src/main/java/ksp/vilius/reddit/service/MailService.java
+++ src/main/java/ksp/vilius/reddit/service/MailService.java
@@ -1,2 +1,38 @@
-package ksp.vilius.reddit.service;public class MailService {
+package ksp.vilius.reddit.service;
+
+import ksp.vilius.reddit.exceptions.SpringRedditException;
+import ksp.vilius.reddit.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendEmail(NotificationEmail email) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(email.getRecipient());
+            messageHelper.setSubject(email.getSubject());
+            messageHelper.setText(email.getBody());
+        };
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!");
+        } catch (MailException e) {
+            log.error("Exception occurred while trying to send email", e);
+            throw new SpringRedditException("Exception occurred when sending email to " + email.getRecipient());
+        }
+    }
+
 }
