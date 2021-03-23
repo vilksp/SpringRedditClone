56d7312c5d542de7efafb8784939424f1a9c7dcf
vilksp <v.kasperavicius1@gmail.com>
2021-03-23 16:47:11 +0200

    Subreddit service added with methods to create subreddit and to get list of all the subreddits

--- src/main/java/ksp/vilius/reddit/service/SubredditService.java
+++ src/main/java/ksp/vilius/reddit/service/SubredditService.java
@@ -1,4 +1,53 @@
 package ksp.vilius.reddit.service;
 
+import ksp.vilius.reddit.dto.SubredditDto;
+import ksp.vilius.reddit.model.Subreddit;
+import ksp.vilius.reddit.repositories.SubredditRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+@Slf4j
 public class SubredditService {
+
+    private final SubredditRepository subredditRepository;
+
+    @Transactional
+    public SubredditDto createSubreddit(SubredditDto subredditDto) {
+        Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+        subredditDto.setId(save.getId());
+        return subredditDto;
+    }
+
+    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
+        return Subreddit.builder()
+                .name(subredditDto.getSubredditName())
+                .description(subredditDto.getDescription())
+                .build();
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubredditDto> getAllSubreddits() {
+
+        return subredditRepository
+                .findAll()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    private SubredditDto mapToDto(Subreddit subreddit) {
+        return SubredditDto.builder()
+                .id(subreddit.getId())
+                .subredditName(subreddit.getName())
+                .description(subreddit.getDescription())
+                .build();
+    }
 }
