ba909c798de480166ddb504ad805a2d7305337c9
vilksp <v.kasperavicius1@gmail.com>
2021-03-23 16:45:48 +0200

    Dto for subbredit added

--- src/main/java/ksp/vilius/reddit/dto/SubredditDto.java
+++ src/main/java/ksp/vilius/reddit/dto/SubredditDto.java
@@ -1,2 +1,18 @@
-package ksp.vilius.reddit.dto;public class SubredditDto {
+package ksp.vilius.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubredditDto {
+
+    private Long id;
+    private String subredditName;
+    private String description;
+    private Integer numberOfPosts;
 }
