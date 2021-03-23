fcfe6a332b71d84a901ad38cb285120c405752e2
vilksp <v.kasperavicius1@gmail.com>
2021-03-23 12:44:24 +0200

    class that implements UserDetails

---
+++ src/main/java/ksp/vilius/reddit/model/SecurityUser.java
@@ -0,0 +1,50 @@
+package ksp.vilius.reddit.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser extends User implements UserDetails {
+
+    public SecurityUser(final User user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
+}
