06d6a1d6a90b2a9e53f7c473c237257dfcda4fc4
vilksp <v.kasperavicius1@gmail.com>
2021-03-23 16:47:24 +0200

    controller for subreddits added

--- src/main/java/ksp/vilius/reddit/controller/SubredditController.java
+++ src/main/java/ksp/vilius/reddit/controller/SubredditController.java
@@ -1,2 +1,34 @@
-package ksp.vilius.reddit.controller;public class SubredditController {
+package ksp.vilius.reddit.controller;
+
+import ksp.vilius.reddit.dto.SubredditDto;
+import ksp.vilius.reddit.service.SubredditService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subreddit")
+@AllArgsConstructor
+@Slf4j
+public class SubredditController {
+
+    private final SubredditService subredditService;
+
+    @PostMapping
+    public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
+
+        return new ResponseEntity<>(subredditService.createSubreddit(subredditDto), HttpStatus.CREATED);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SubredditDto>> getAllSubreddit() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(subredditService.getAllSubreddits());
+    }
 }
