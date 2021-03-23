cf3d83d9d3c6e00d476947779de88c0d849b42a2
vilksp <v.kasperavicius1@gmail.com>
2021-03-23 14:30:12 +0200

    Security for jwt

--- src/main/java/ksp/vilius/reddit/security/JwtAuthenticationFilter.java
+++ src/main/java/ksp/vilius/reddit/security/JwtAuthenticationFilter.java
@@ -1,2 +1,59 @@
-package ksp.vilius.reddit.security;public class JwtAuthenticationFilter {
+package ksp.vilius.reddit.security;
+
+import ksp.vilius.reddit.model.SecurityUser;
+import ksp.vilius.reddit.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtProvider tokenProvider;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwt = getJwtFromRequest(request);
+
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            //Extract user id from jwt token
+            String username = tokenProvider.getUsernameFromJwt(jwt);
+
+            SecurityUser user = (SecurityUser) userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            auth.setDetails(new WebAuthenticationDetailsSource()
+                    .buildDetails(request));
+            //If everything goes fine, set authentication to Security context holder
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        } else {
+
+
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+
+        String header = request.getHeader("Authorization");
+
+        if (org.springframework.util.StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
 }
--- src/main/java/ksp/vilius/reddit/security/JwtProvider.java
+++ src/main/java/ksp/vilius/reddit/security/JwtProvider.java
@@ -1,2 +1,77 @@
-package ksp.vilius.reddit.security;public class JwtProvider {
+package ksp.vilius.reddit.security;
+
+import io.jsonwebtoken.*;
+import ksp.vilius.reddit.model.SecurityUser;
+import ksp.vilius.reddit.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+
+
+    String secret = "3207C63AFF1E40703056B951343BD22C16851D3ED2D5DA257AB094E71BBD1712";
+
+    public String generateToken(Authentication authentication) {
+
+        User user = (SecurityUser) authentication.getPrincipal();
+
+        long expire = 60 * 60 * 24 * 7 * 1000;
+
+
+        Date now = new Date(System.currentTimeMillis());
+        Date expiry = new Date(now.getTime() + expire);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        claims.put("id", user.getUserId());
+        return Jwts.builder()
+                .setSubject(user.getUserId().toString())
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .setIssuer("Spring reddit clone")
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return !claimsJws.getBody().getExpiration().before(new Date());
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException |
+                IllegalArgumentException e) {
+            throw new RuntimeException("Invalid token");
+        }
+    }
+
+
+    public Long getUserIdFromJwt(String token) {
+        Claims claim = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+
+        String id = "";
+        try {
+            id = (String) claim.get("id");
+        } catch (Exception e) {
+            return Long.valueOf(claim.get("id").toString());
+        }
+        return Long.parseLong(id);
+    }
+
+    public String getUsernameFromJwt(String token) {
+        Claims claim = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+
+        String username = "";
+        try {
+            username = (String) claim.get("username");
+        } catch (Exception e) {
+            return claim.get("username").toString();
+        }
+        return username;
+    }
 }
