798a6377b622eccbb32e6a3c6857665f6d36301c
vilksp <v.kasperavicius1@gmail.com>
2021-03-25 14:29:57 +0200

    new exception

--- src/main/java/ksp/vilius/reddit/exceptions/SubredditNotFoundException.java
+++ src/main/java/ksp/vilius/reddit/exceptions/SubredditNotFoundException.java
@@ -1,6 +1,7 @@
 package ksp.vilius.reddit.exceptions;
 
-public class SubredditNotFoundException extends X {
-    public SubredditNotFoundException(String no_such_subreddit) {
+public class SubredditNotFoundException extends RuntimeException {
+    public SubredditNotFoundException(String message) {
+        super(message);
     }
 }
