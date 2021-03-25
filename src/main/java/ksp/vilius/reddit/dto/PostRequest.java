86311a4ce3c9489ecf713a4247c3fa3cbc85d836
vilksp <v.kasperavicius1@gmail.com>
2021-03-25 14:29:18 +0200

    postRequest and response dtos added

--- src/main/java/ksp/vilius/reddit/dto/PostRequest.java
+++ src/main/java/ksp/vilius/reddit/dto/PostRequest.java
@@ -1,4 +1,16 @@
 package ksp.vilius.reddit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostRequest {
+    private Long postId;
+    private String subredditName;
+    private String postName;
+    private String url;
+    private String description;
 }
--- src/main/java/ksp/vilius/reddit/dto/PostResponse.java
+++ src/main/java/ksp/vilius/reddit/dto/PostResponse.java
@@ -1,2 +1,22 @@
-package ksp.vilius.reddit.dto;public class PostResponse {
+package ksp.vilius.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+    private Long id;
+    private String postName;
+    private String url;
+    private String description;
+    private String userName;
+    private String subredditName;
+    private Integer voteCount;
+    private Integer commentCount;
+    private String duration;
+    private boolean upVote;
+    private boolean downVote;
 }
