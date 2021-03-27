4e776ac95004f4dfe2fbcb28e5d1b383ce696968
vilksp <v.kasperavicius1@gmail.com>
2021-03-27 19:09:15 +0200

    custom method to obtain most recend vote

--- src/main/java/ksp/vilius/reddit/repositories/VoteRepository.java
+++ src/main/java/ksp/vilius/reddit/repositories/VoteRepository.java
@@ -1,9 +1,14 @@
 package ksp.vilius.reddit.repositories;
 
+import ksp.vilius.reddit.model.Post;
+import ksp.vilius.reddit.model.User;
 import ksp.vilius.reddit.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
